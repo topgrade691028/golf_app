178d2f1ae619686a684850785ddc9e4ecaa824ed
topgrade691028 <pronane@yahoo.com>
2023-04-23 22:20:54 +0100

    Updating imports

--- src/main/java/com/pr/golf/golfapp/model/Hole.java
+++ src/main/java/com/pr/golf/golfapp/model/Hole.java
@@ -1,17 +1,16 @@
 package com.pr.golf.golfapp.model;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Transient;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * | ________________________________________________
