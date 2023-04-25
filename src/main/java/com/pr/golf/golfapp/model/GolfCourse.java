7cd81fd7abf7f93814502e597724717e16eb19d5
topgrade691028 <pronane@yahoo.com>
2023-04-25 18:45:20 +0100

    adding for a ply around with ChatGPT

--- src/main/java/com/pr/golf/golfapp/model/GolfCourse.java
+++ src/main/java/com/pr/golf/golfapp/model/GolfCourse.java
@@ -3,16 +3,10 @@ package com.pr.golf.golfapp.model;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
